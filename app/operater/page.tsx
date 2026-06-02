43dfc46a8398560dd9f0465f78b4050a295ab1cb
sobaaaaan <notepc_repair@yahoo.co.jp>
2026-06-02 14:32:39 +0900

    Update page.tsx

--- app/operater/page.tsx
+++ app/operater/page.tsx
@@ -73,7 +73,7 @@ export default function Page() {
           </tr>
           <tr>
             <th style={thStyle}>運営者</th>
-            <td style={tdStyle}>株式会社スクリーンプラス</td>
+            <td style={tdStyle}>すくぷら工房</td>
           </tr>
           <tr>
             <th style={thStyle}>所在地</th>
